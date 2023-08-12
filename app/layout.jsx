d2de997a4b39ea5cda554c0525533a60face82a7
Gilbert Hyman Goes <gilberthg@fairtech.com.sg>
2023-08-12 18:40:04 +0800

    Implement homepage & create prompt page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css';
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 export const metadata = {
     title: "Promptopia",
     description: "Discover & Shares AI Prompts"
@@ -9,13 +12,16 @@ const RootLayout = ({ children }) => {
     return (
         <html lang={"en"}>
             <body>
+                <Provider>
                 <div className={"main"}>
                     <div className={"gradient"}/>
                 </div>
 
                 <main className={"app"}>
+                    <Nav />
                     {children}
                 </main>
+                </Provider>
             </body>
         </html>
     );
